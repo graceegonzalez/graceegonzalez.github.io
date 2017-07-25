179bdbe3bd9e205e1d5b174e94741289bfa4b4e6
graceegonzalez <gracee.gonzalez6@gmail.com>
2017-07-25 15:39:41 -0400

    Added scrolling photos

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -2,3 +2,21 @@ window.alert("Welcome To Grace Gonzalez's Portfolio");
 function styleAbout() {
   document.getElementById("styleAbout").innerHTML = "A favorite pastime of mine is photography!";
 }
+
+var slideIndex = 1;
+showIMG(slideIndex);
+
+function plusIMG(n) {
+    showIMG(slideIndex += n);
+}
+
+function showIMG(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
